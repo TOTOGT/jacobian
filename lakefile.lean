b09731f329d7418fc2b33d40502bce8a04b0a21e
Pablo N Grossi <brodananda@gmail.com>
2026-07-24 16:55:36 -0400

    Initialize Lake configuration for Jacobian package

--- lakefile.lean
+++ lakefile.lean
@@ -1,1 +1,14 @@
-v.4.32.1
+import Lake
+open Lake DSL
+
+package «jacobian» {
+  -- Add any package configuration options here
+}
+
+@[default_target]
+lean_lib «Jacobian» {
+  -- Add any library configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
